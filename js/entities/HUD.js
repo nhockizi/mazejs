b1abb66eaae95bb205205edb0a7007a3c45a9265
NhocKizi <nhockizi@gmail.com>
2015-08-06 14:58:43 +0700

    a

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -1,11 +1,8 @@
-
-
 /**
  * a HUD container and child items
  */
 
 game.HUD = game.HUD || {};
-
 
 game.HUD.Container = me.Container.extend({
     init: function() {
@@ -22,7 +19,8 @@ game.HUD.Container = me.Container.extend({
         this.name = "HUD";
 
         // add our child score object at position
-        this.addChild(new game.HUD.ScoreItem("score", "left", 10, 10));
+        this.addChild(new game.HUD.ScoreItem("score", "left", 650, 10));
+
     }
 });
 
@@ -36,22 +34,14 @@ game.HUD.ScoreItem = me.Renderable.extend({
      */
     init: function(score, align, x, y) {
 
-        this.relative = new me.Vector2d(x, y);
-
-        // call the super constructor
+        // call the super constructor 
         // (size does not matter here)
-        this._super(me.Renderable, 'init', [
-            me.game.viewport.width + x,
-            me.game.viewport.height + y,
-            10,
-            10
-        ]);
+        this._super(me.Renderable, 'init', [x, y, 10, 10]);
 
         // create a font
-        this.font = new me.BitmapFont("atascii", {x:24});
+        this.font = new me.BitmapFont("atascii", {x: 24});
         this.font.alignText = "bottom";
-        this.font.set("right", 1.6);
-        
+        this.font.set(align, 1.2);
         // ref to the score variable
         this.scoreRef = score;
 
@@ -62,7 +52,78 @@ game.HUD.ScoreItem = me.Renderable.extend({
      * draw the score
      */
     draw: function(context) {
-//        this.font.draw(context, game.data[this.scoreRef], this.pos.x, this.pos.y);
+        this.font.draw(context, game.data[this.scoreRef], this.pos.x, this.pos.y);
     }
+});
+game.HUD.TimerManager = me.Entity.extend(
+        {
+            init: function()
+            {
+                // call the super constructor
+                var settings = {};
+                settings.width = 10;
+                settings.height = 10;
+                // call the super constructor
+                this.complete = false;
+                this._super(me.Container, 'init');
+
+                // persistent across level change
+                this.isPersistent = true;
+
+                // make sure our object is always draw first
+                this.z = Infinity;
+                // 5 minutes in milliseconds, count down to true
+                this.timer = new game.HUD.TimerItem(2 * 60 * 1000, true, 200, 10, "timer");
+		me.game.world.addChild(this.timer);
+            },
+            update: function()
+            {
+                
+            }
+        });
+game.HUD.TimerItem = me.Renderable.extend({
+    init: function(time, countdown, x, y, name, align) {
+        // call the constructor
+        this._super(me.Renderable, 'init', [x, y, 10, 10])
+        // create a font
+        this.font = new me.Font("Arial", 32, "#EE0000");
+        this.font.textAlign = "center";
+        // give a name
+        this.name = name;
+        this.pos.x = x;
+        this.pos.y = y;
+        this.time = time;
+        this.time_remain = time;
+        this.countdown = countdown;
+        this.start_time = me.timer.getTime();
+    },
+    draw: function(ctx)
+	{
+        var context = ctx.getContext();
+        this.font.draw(context, this.value, this.pos.x, this.pos.y);
+	},
+    convert: function(time_remain)
+    {
+        var x = parseInt(time_remain / 1000);
+        var seconds = x % 60;
+        x /= 60;
+        var minutes = x % 60;
+
+        var seconds_num = minutes * 60 + seconds;
+        console.log(seconds_num);
+        if (seconds_num > 0) {
+            var aa = Math.floor(minutes) + ":" + Math.floor(seconds);
+            console.log(aa);
+            return Math.floor(minutes) + ":" + Math.floor(seconds);
+        }
+        else {
+            return "0:0";
+        }
+    },
+    update: function(dt)
+    {
+        
+    }
+
 
 });
